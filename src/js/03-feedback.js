d0799507b9c22d73634dcce11818ae5c6b6620da
dmistori84 <81422874+dmistori84@users.noreply.github.com>
2023-05-20 12:07:59 +0300

    3 task done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from "lodash.throttle";
+
+const refs = {
+    formEl: document.querySelector('.feedback-form'),
+    inputEmail: document.querySelector('[name=email]'),
+    textarea: document.querySelector('[name=message]')
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+refs.formEl.addEventListener('submit', onFormSumbit);
+refs.formEl.addEventListener('input', throttle(onInput), 500);
+
+loadDataFromLocalStorage();
+
+function onFormSumbit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
+ 
+function onInput(event) {
+    formData[event.target.name] = event.target.value;
+    const formDataStrf = JSON.stringify(formData);
+    localStorage.setItem(STORAGE_KEY, formDataStrf);
+};
+
+function loadDataFromLocalStorage() {
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    if (savedData) {
+        refs.inputEmail.value = savedData.email;
+        refs.textarea.value = savedData.message;
+    }
+};
 
